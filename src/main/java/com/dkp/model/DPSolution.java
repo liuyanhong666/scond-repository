c2a562887703327ffdd9376deb087eaf48946028
3307315034@qq.com <3307315034@qq.com>
2026-03-23 12:14:37 +0800

    feat: 添加Item和ItemSet实体类，完成数据模型

--- src/main/java/com/dkp/model/DPSolution.java
+++ src/main/java/com/dkp/model/DPSolution.java
@@ -1,76 +1,20 @@
 package com.dkp.model;
 
-import java.util.Arrays;
-
-/**
- * 动态规划求解结果类
- * 封装最优解价值和选择向量
- * 
- * @author DKP Solver Team
- * @version 1.0
- */
 public class DPSolution {
-    
-    /** 最优解价值 */
     private int optimalValue;
-    
-    /** 选择向量，每个元素表示对应项集选择的物品索引（1-3，0表示不选） */
     private int[] selectedItems;
-    
-    /** 求解耗时（毫秒） */
     private long solveTime;
     
-    /**
-     * 构造函数
-     * @param optimalValue 最优解价值
-     * @param selectedItems 选择向量
-     */
-    public DPSolution(int optimalValue, int[] selectedItems) {
-        this.optimalValue = optimalValue;
-        this.selectedItems = selectedItems;
-        this.solveTime = 0;
-    }
-    
-    /**
-     * 构造函数（带耗时）
-     * @param optimalValue 最优解价值
-     * @param selectedItems 选择向量
-     * @param solveTime 求解耗时
-     */
     public DPSolution(int optimalValue, int[] selectedItems, long solveTime) {
         this.optimalValue = optimalValue;
         this.selectedItems = selectedItems;
         this.solveTime = solveTime;
     }
     
-    public int getOptimalValue() {
-        return optimalValue;
-    }
+    public int getOptimalValue() { return optimalValue; }
+    public int[] getSelectedItems() { return selectedItems; }
+    public long getSolveTime() { return solveTime; }
     
-    public void setOptimalValue(int optimalValue) {
-        this.optimalValue = optimalValue;
-    }
-    
-    public int[] getSelectedItems() {
-        return selectedItems;
-    }
-    
-    public void setSelectedItems(int[] selectedItems) {
-        this.selectedItems = selectedItems;
-    }
-    
-    public long getSolveTime() {
-        return solveTime;
-    }
-    
-    public void setSolveTime(long solveTime) {
-        this.solveTime = solveTime;
-    }
-    
-    /**
-     * 获取格式化的解向量字符串
-     * @return 解向量字符串
-     */
     public String getFormattedSolution() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < selectedItems.length; i++) {
@@ -81,11 +25,5 @@ public class DPSolution {
             }
         }
         return sb.toString();
-    }
-    
-    @Override
-    public String toString() {
-        return String.format("DPSolution{optimalValue=%d, selectedItems=%s, solveTime=%dms}",
-                optimalValue, Arrays.toString(selectedItems), solveTime);
     }
 }
--- src/main/java/com/dkp/model/Item.java
+++ src/main/java/com/dkp/model/Item.java
@@ -1,59 +1,18 @@
 package com.dkp.model;
 
-/**
- * 物品实体类
- * 封装物品的重量和价值属性
- * 
- * @author DKP Solver Team
- * @version 1.0
- */
 public class Item {
-    
-    /** 物品重量 */
     private int weight;
-    
-    /** 物品价值 */
     private int value;
     
-    /**
-     * 构造函数
-     * @param weight 物品重量
-     * @param value 物品价值
-     */
     public Item(int weight, int value) {
         this.weight = weight;
         this.value = value;
     }
     
-    public int getWeight() {
-        return weight;
-    }
+    public int getWeight() { return weight; }
+    public int getValue() { return value; }
     
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-    
-    public int getValue() {
-        return value;
-    }
-    
-    public void setValue(int value) {
-        this.value = value;
-    }
-    
-    /**
-     * 获取价值重量比
-     * @return 价值与重量的比值
-     */
     public double getValueWeightRatio() {
-        if (weight == 0) {
-            return 0.0;
-        }
-        return (double) value / weight;
-    }
-    
-    @Override
-    public String toString() {
-        return "Item{weight=" + weight + ", value=" + value + "}";
+        return weight == 0 ? 0 : (double) value / weight;
     }
 }
